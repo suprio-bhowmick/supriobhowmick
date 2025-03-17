d1fb291ed5dba568224f44c200b06c4af6a275df
suprio-bhowmick <supriobhowmick2025@gmail.com>
2025-03-17 15:58:00 +0530

    update meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,11 +10,11 @@ const outfit = Outfit({
 export const metadata: Metadata = {
   title: "Suprio Bhowmick | React Developer Portfolio",
   description:
-    "Welcome to the portfolio of Suprio Bhowmick , a React.js developer skilled in building modern web applications. Check out my projects, skills, and contact me for collaboration.",
+    "Welcome to the portfolio of Suprio Bhowmick, a React.js developer skilled in building modern web applications. Check out my projects, skills, and contact me for collaboration.",
   keywords:
     "React Developer, Frontend Developer, Web Developer, JavaScript, React.js Portfolio, Next.js, UI/UX, MERN Stack, Freelancer, Hire React Developer",
-  authors: [{ name: "Suprio Bhowmick " }],
-  creator: "Suprio Bhowmick ",
+  authors: [{ name: "Suprio Bhowmick" }], 
+  creator: "Suprio Bhowmick",
   openGraph: {
     title: "Suprio Bhowmick | React Developer Portfolio",
     description:
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
     siteName: "Suprio Bhowmick | React Developer",
     images: [
       {
-        url: "https://opengraph.b-cdn.net/production/images/7dc56029-0fee-4d5f-9972-b3c22c5f6dbd.png?token=OddpILhM-mICvX56BeWz5WWSa89_57UQ0DMWKn7lOLI&height=564&width=1200&expires=33278206076",
+        url: "https://supriobhowmick.vercel.app/images/portfolio-preview.png", 
         width: 1200,
         height: 630,
         alt: "React Developer Portfolio Preview",
@@ -33,15 +33,15 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "React Developer Portfolio | Your Name",
+    title: "Suprio Bhowmick | React Developer Portfolio", 
     description:
       "React.js developer portfolio showcasing projects, skills, and achievements. Let's connect!",
-    images: ["https://opengraph.b-cdn.net/production/images/7dc56029-0fee-4d5f-9972-b3c22c5f6dbd.png?token=OddpILhM-mICvX56BeWz5WWSa89_57UQ0DMWKn7lOLI&height=564&width=1200&expires=33278206076"],
+    images: ["https://supriobhowmick.vercel.app/images/portfolio-preview.png"],
     creator: "@yourtwitterhandle",
   },
   robots: "index, follow",
-
 };
+
 
 export default function RootLayout({
   children,
